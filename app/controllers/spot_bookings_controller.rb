d32ea1b029d94a1a4047ffeb1ba6e9eec6681cbf
Lenny Knoop <lennartpknoop@gmail.com>
2018-11-19 18:22:44 +0100

    made fix to bookings controller

--- app/controllers/spot_bookings_controller.rb
+++ app/controllers/spot_bookings_controller.rb
@@ -1,14 +1,14 @@
 class SpotBookingsController < ApplicationController
   def new
-    @spot_booking = SpotReview.new
+    @spot_booking = SpotBookings.new
   end
 
   def create
-    @spot_booking = SpotReview.create(spot_review_params)
+    @spot_booking = SpotBookings.create(spot_booking_params)
     redirect_to spot_path(@spot_booking.spot) # will this work? find out next week on ...
   private
 
-  def spot_review_params
+  def spot_booking_params
     params.require(:spot_booking).permit(:booking_date, :is_approved)
   end
 
