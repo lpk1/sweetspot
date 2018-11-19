69d009a88352dbf6234535c0bafdf2cdedff61cd
rhett buettrich <rhett.buettrich@hotmail.com>
2018-11-19 17:15:18 +0100

    Routing finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :spots do
+    resources :spot_bookings, only: %i[create, new]
+    resources :spot_reviews, only: %i[create, new]
+  end
+
+  resources :users, only: [:show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
