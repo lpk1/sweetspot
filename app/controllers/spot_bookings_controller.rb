f4dbdb9b13be085ac3a15d7301907cdb53527245
Lenny Knoop <lennartpknoop@gmail.com>
2018-11-21 10:58:36 +0100

    fixed bookings page

--- app/controllers/spot_bookings_controller.rb
+++ app/controllers/spot_bookings_controller.rb
@@ -1,15 +1,24 @@
 class SpotBookingsController < ApplicationController
   def new
-    @spot_booking = SpotBookings.new
+    @spot = Spot.find(params[:spot_id])
+    @spot_booking = SpotBooking.new
+    authorize @spot
   end
 
   def create
-    @spot_booking = SpotBookings.create(spot_booking_params)
-    redirect_to spot_path(@spot_booking.spot) # will this work? find out next week on ...
+    @spot = Spot.find(params[:spot_id])
+    @spot_booking = SpotBooking.new(spot_booking_params)
+    @spot_booking.user = current_user
+    @spot_booking.spot = @spot
+    @spot_booking.save
+
+    authorize @spot
+  end
+
   private
 
   def spot_booking_params
-    params.require(:spot_booking).permit(:booking_date, :is_approved)
+    params.require(:spot_booking).permit(:start_date, :end_date, :is_approved)
   end
 
 end
