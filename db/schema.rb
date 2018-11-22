9b606adb72f32a27f1066452877c78bedb69b2d3
Lenny Knoop <lennartpknoop@gmail.com>
2018-11-22 11:37:09 +0100

    adjust show page and added status.

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2018_11_19_134129) do
     t.bigint "user_id"
     t.date "start_date"
     t.date "end_date"
-    t.boolean "is_approved"
+    t.boolean "is_approved", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["spot_id"], name: "index_spot_bookings_on_spot_id"
