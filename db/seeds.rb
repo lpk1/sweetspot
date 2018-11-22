73d463a87cec4b8d099e82180d04ee8bb9b2c952
Tim Wehrmeyer <tim.wehrmeyer@gmail.com>
2018-11-22 15:02:12 +0100

    structured file

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,27 +17,57 @@ user2 = User.create(email: "christmaspowder@anonymous.com", password: "847463")
 user3 = User.create(email: "eastereggs@jamiesidea.com", password: "4829575")
 user4 = User.create(email: "lenny@me.com", password: "123456")
 
-spot1 = Spot.new(name: "Miami Beach", address: "Miami Beach", description: "Hi there this is a nice place in miami for camping.")
+spot1 = Spot.new(
+  name: "Miami Beach",
+  address: "Miami Beach",
+  description: "Hi there this is a nice place in miami for camping.",
+  photo: open()
+  )
 spot1.user = user1
 spot1.save
 
-spot2 = Spot.new(name: "Schwepnitz", address: "Nesthakenweg 9", description: "Southpole. Flat out stunning landscape. Haven't found an single eis-beergen")
+spot2 = Spot.new(
+  name: "Schwepnitz",
+  address: "Nesthakenweg 9",
+  description: "Southpole. Flat out stunning landscape.",
+  photo: open()
+  )
 spot2.user = user1
 spot2.save
 
-spot3 = Spot.new(name: "Munich", address: "Karlstrasse 35",  description: "Bavariaaaaaaa. It has beeeeer")
+spot3 = Spot.new(
+  name: "Munich",
+  address: "Karlstrasse 35",
+  description: "Bavariaaaaaaa. It has beeeeer",
+  photo: open()
+  )
 spot3.user = user3
 spot3.save
 
-spot4 = Spot.new(name: "Berlin", address: "rudi-dutschke-strasse 60",  description: "Berlin. City of awsome. Is this enough characters?")
+spot4 = Spot.new(
+  name: "Berlin",
+  address: "rudi-dutschke-strasse 60",
+  description: "Berlin. City of awsome. Is this enough characters?",
+  photo: open()
+  )
 spot4.user = user3
 spot4.save
 
-spot5 = Spot.new(name: "Coin, Spain", address: "Plaza de la Alameda",  description: "Santa Cruz. Home town to crazy mfkrs.")
+spot5 = Spot.new(
+  name: "Coin, Spain",
+  address: "Plaza de la Alameda",
+  description: "Santa Cruz. Home town to crazy mfkrs.",
+  photo: open()
+  )
 spot5.user = user2
 spot5.save
 
-spot6 = Spot.new(name: "Lennys Crib", address: "@Lennys - The greatest place", latitude: rand() * 90, longitude: rand() * 180, description: "Everyone is welcome at all times. Requirements include a sixpack of Corona.")
+spot6 = Spot.new(
+  name: "Lennys Crib",
+  address: "Cote d'azur",
+  description: "Everyone is welcome at all times. Requirements include a sixpack of Corona. Yes, his taste is aweful."
+  photo: open()
+  )
 spot6.user = user4
 spot6.save
 
