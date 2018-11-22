9a5bcf1d4cdef3ef1f727b3924df2b92c5f61c49
Lenny Knoop <lennartpknoop@gmail.com>
2018-11-22 15:17:57 +0100

    removed redirect_to index if logged in.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,3 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  before_action :require_login
-
-  def home
-  end
-
-  def require_login
-    if user_signed_in?
-      redirect_to spots_path
-    end
-  end
 end
