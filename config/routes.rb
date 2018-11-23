1665adc491171b3f24e0a946623cf10715c7e35c
Jamie Brouwer <jamesonbrouwer@gmail.com>
2018-11-23 11:40:12 +0100

    changed links to images

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   get 'users/bookings', to: "users#bookings", as: :user_show_bookings
   get 'users/listings', to: "users#listings", as: :user_show_listings
+  get 'users/profile', to: "users#profile", as: :user_show_profile
 
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
